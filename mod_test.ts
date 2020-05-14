202fe2b47700977cabb6070e006eacb70fdc5e18
Anand Chowdhary <mail@anandchowdhary.com>
2020-05-14 20:44:51 +0530

    :green_heart: Test correct result on Windows

--- mod_test.ts
+++ mod_test.ts
@@ -3,6 +3,14 @@ import { recursiveReaddir } from "./mod.ts";
 
 Deno.test("test starter function", async (): Promise<void> => {
   const result = await recursiveReaddir("testfiles");
-  const correctResult = ["testfiles/file.txt", "testfiles/to/file.txt"];
-  assertEquals(result.sort(), correctResult.sort());
+  const correctResultUnix = ["testfiles/file.txt", "testfiles/to/file.txt"];
+  const correctResultWindows = [
+    "testfiles\\file.txt",
+    "testfiles\\to\\file.txt",
+  ];
+  try {
+    assertEquals(result.sort(), correctResultUnix.sort());
+  } catch (error) {
+    assertEquals(result.sort(), correctResultWindows.sort());
+  }
 });
